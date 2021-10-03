0b42dd89a66fb2f7e70ddb688ebe0ae6086f43ef
GilangSebastian <gilangsm2024@gmail.com>
2021-10-03 09:34:56 +0700

    add rak router into main router

--- routers/index.js
+++ routers/index.js
@@ -4,10 +4,12 @@ const mainRouter = require("express").Router();
 const transaksiRouter = require("./transaksi");
 const userRouter = require("./user");
 const lemariRouter = require("./lemari");
+const rakRouter = require("./rak");
 
 
 mainRouter.use(transaksiRouter);
 mainRouter.use(userRouter);
-mainRouter.use(lemariRouter)
+mainRouter.use(lemariRouter);
+mainRouter.use(rakRouter);
 
 module.exports = mainRouter;
